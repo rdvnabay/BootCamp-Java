90c079df78f142326d3fd9e9531fdb7d5c2cd1b7
Rıdvan <ridvanabay@gmail.com>
2021-05-22 22:50:54 +0300

    7.Gün - 2.Ara (Result, IDataResult)

---
+++ day6_northwind/src/main/java/kodlamaio/day6_northwind/core/utilities/results/ErrorResult.java
@@ -0,0 +1,11 @@
+package kodlamaio.day6_northwind.core.utilities.results;
+
+public class ErrorResult extends Result {
+
+    public ErrorResult() {
+        super(false);
+    }
+    public ErrorResult(String message) {
+        super(false,message);
+    }  
+}
---
+++ day6_northwind/src/main/java/kodlamaio/day6_northwind/core/utilities/results/SuccessResult.java
@@ -0,0 +1,12 @@
+package kodlamaio.day6_northwind.core.utilities.results;
+
+public class SuccessResult extends Result {
+
+    public SuccessResult() {
+    super(true);
+    }
+
+    public SuccessResult(String message) {
+    super(true,message);
+    }
+}
