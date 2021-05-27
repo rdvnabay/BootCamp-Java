067eb7ee1f784bfc859ef7288e16d2eff4389f51
Rıdvan <ridvanabay@gmail.com>
2021-05-27 21:56:37 +0300

    8.Gün  - 1.Ara (One to Many Relationship)

--- day6_northwind/src/main/java/kodlamaio/day6_northwind/dataAccess/abstracts/ProductDao.java
+++ day6_northwind/src/main/java/kodlamaio/day6_northwind/dataAccess/abstracts/ProductDao.java
@@ -1,7 +1,19 @@
 package kodlamaio.day6_northwind.dataAccess.abstracts;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import kodlamaio.day6_northwind.entities.concretes.Product;
 
 public interface ProductDao extends JpaRepository<Product,Integer>{
+Product getByProductName(String productName);
+Product getByProductNameAndCategoryId(String productName,int categoryId);
+List<Product> getByProductNameOrCategoryId(String productName,int categoryId);
+List<Product> getByCategoryIdIn(List<Integer> categories);
+List<Product> getProductNameContains(String productName);
+List<Product> getProductNameStartsWith(String productName);
 
+@Query("From Product where productName=:productName and categoryId=:categorydId")
+List<Product> getByNameAndCategory(String productName,int categoryId);
 }
