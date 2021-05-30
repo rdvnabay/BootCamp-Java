183a0fe276f4f14d0908e8957e2fe3444e9d3dad
Rıdvan <ridvanabay@gmail.com>
2021-05-31 00:34:19 +0300

    9.Gün - Son Bölüm

--- day6_northwind/src/main/java/kodlamaio/day6_northwind/api/UsersController.java
+++ day6_northwind/src/main/java/kodlamaio/day6_northwind/api/UsersController.java
@@ -1,35 +1,57 @@
 package kodlamaio.day6_northwind.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import kodlamaio.day6_northwind.business.abstracts.UserService;
 import kodlamaio.day6_northwind.core.entities.User;
 import kodlamaio.day6_northwind.core.utilities.results.DataResult;
+import kodlamaio.day6_northwind.core.utilities.results.ErrorDataResult;
 
 @RestController
 @RequestMapping("/api/users")
 public class UsersController {
-    
+
     @Autowired
     private UserService userService;
+
     public UsersController(UserService userService) {
         super();
         this.userService = userService;
     }
 
     @PostMapping("/add")
-    public ResponseEntity<?> add(@RequestBody User user){
+    public ResponseEntity<?> add(@Valid @RequestBody User user) {
         return ResponseEntity.ok(this.userService.add(user));
     }
 
     @GetMapping("/getbyemail")
-    public ResponseEntity<DataResult<User>> getByEmail(String email){
+    public ResponseEntity<DataResult<User>> getByEmail(String email) {
         return ResponseEntity.ok(this.userService.getByEmail(email));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions) {
+        Map<String, String> validationErrors = new HashMap<String, String>();
+        for (FieldError fieldError : exceptions.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        ErrorDataResult<Object> errors=new ErrorDataResult<Object>(validationErrors,"Doğrulama hataları");
+    return errors;
     }
 }
