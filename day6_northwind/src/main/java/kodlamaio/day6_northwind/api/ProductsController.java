7e9341d6ed5c30c6d817d9b8e3f0d3feb1be9959
Rıdvan <ridvanabay@gmail.com>
2021-06-06 00:58:52 +0300

    11.Gün - Son (Cors Yapılandırması)

--- day6_northwind/src/main/java/kodlamaio/day6_northwind/api/ProductsController.java
+++ day6_northwind/src/main/java/kodlamaio/day6_northwind/api/ProductsController.java
@@ -3,6 +3,7 @@ package kodlamaio.day6_northwind.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin
 public class ProductsController {
 
    @Autowired
